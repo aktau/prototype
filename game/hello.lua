24d62d659f9bdcce11b4c9db697cfc5817238b52
Aktau <nicolashillegeer@gmail.com>
2013-08-20 15:20:09 +0200

    test LuaJIT ffi, seems to work (more than) wonderfully

--- game/hello.lua
+++ game/hello.lua
@@ -1,3 +1,22 @@
+-- load the ffi library
+local ffi = require("ffi")
+
+ffi.cdef[[
+void Sleep(int ms);
+int poll(struct pollfd *fds, unsigned long nfds, int timeout);
+]]
+
+local sleep
+if ffi.os == "Windows" then
+  function sleep(s)
+    ffi.C.Sleep(s*1000)
+  end
+else
+  function sleep(s)
+    ffi.C.poll(nil, 0, s*1000)
+  end
+end
+
 -- defines a factorial function
 function fact (n)
     if n == 0 then
@@ -12,5 +31,10 @@ local moar = 10
 
 print("lua interpreter compatibility version: " .. tostring(_VERSION))
 print("jit status: " .. tostring(jit.status()))
-print("The facturial of " .. fn .. " is " .. fact(fn))
-print("The facturial of " .. moar .. " is " .. fact(moar))
+io.write("luajit ffi: ")
+io.flush()
+sleep(0.01)
+print("works");
+
+print("The factorial of " .. fn .. " is " .. fact(fn))
+print("The factorial of " .. moar .. " is " .. fact(moar))
