1f4e9e8c753decd5c215e89d0baa5f628d1de7c2
Nicolas Hillegeer <nicolas@hillegeer.com>
2013-12-30 21:26:23 +0100

    add a static assert macro
    Have to find some sort equivalent for msvc later...

--- src/util.h
+++ src/util.h
@@ -33,6 +33,7 @@
 #include "gfx.h"
 
 #define PACKED __attribute__((packed))
+#define STATIC_ASSERT _Static_assert
 
 #define likely(x)       __builtin_expect(!!(x), 1)
 #define unlikely(x)     __builtin_expect(!!(x), 0)
