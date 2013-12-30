f2d6397f2be983c24781367322206e248a0917c1
Nicolas Hillegeer <nicolas@hillegeer.com>
2013-12-30 12:07:03 +0100

    mathfun: ignore extra GCC warning
    Don't want to edit the source, since it works. Just ignore
    warnings as they appear in that file.

--- src/math/sse_mathfun.h
+++ src/math/sse_mathfun.h
@@ -38,6 +38,7 @@
 #elif defined(__GNUC__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wfloat-conversion"
 #pragma GCC diagnostic ignored "-Wcast-align"
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #endif
